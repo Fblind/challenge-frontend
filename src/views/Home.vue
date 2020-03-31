847cdac672767e183bdc1a9ca19351675eae2a7f
Fblind <fblind@gmail.com>
2020-03-31 12:13:01 -0300

    Use store to get mocked posts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <v-content>
-    <Post/>
+    <Post :post="post" v-for="post of posts" :key="post.id"/>
   </v-content>
 </template>
 
 <script>
 // @ is an alias to /src
 import Post from '@/components/Post.vue'
+import {mapState} from "vuex"
 
 export default {
   name: 'Home',
   components: {
     Post
-  }
+  },
+  computed: {...mapState(["posts"])}
 }
 </script>
